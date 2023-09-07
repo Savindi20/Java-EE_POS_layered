e40c60918324ec000ceb502882b0f6e434238a0e
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-07 19:57:30 +0530

    dao factory added

---
+++ BackEnd/src/lk/ijse/pos/dao/DAOFactory.java
@@ -0,0 +1,14 @@
+package lk.ijse.pos.dao;
+
+public class DAOFactory {
+    private static DAOFactory daoFactory;
+
+    private DAOFactory() {
+
+    }
+
+    public static DAOFactory getInstance() {
+        return daoFactory == null ? new DAOFactory() : daoFactory;
+    }
+
+}
