64cd1d17faaa42688d758a7fb418b41be6dc8973
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-07 11:58:34 +0530

    item servlet updated with json

--- BackEnd/src/lk/ijse/pos/controller/servlet/ItemServlet.java
+++ BackEnd/src/lk/ijse/pos/controller/servlet/ItemServlet.java
@@ -1,16 +1,169 @@
 package lk.ijse.pos.controller.servlet;
 
+import javax.json.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
 
 @WebServlet(urlPatterns = "/item")
 public class ItemServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Do Get Item");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/posapi", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Item");
+            ResultSet rst = pstm.executeQuery();
+            JsonArrayBuilder allItems = Json.createArrayBuilder();
+
+            while (rst.next()) {
+                JsonObjectBuilder item = Json.createObjectBuilder();
+                item.add("code", rst.getString("code"));
+                item.add("description", rst.getString("description"));
+                item.add("qtyOnHand", rst.getString("qtyOnHand"));
+                item.add("unitPrice", rst.getDouble("unitPrice"));
+                allItems.add(item.build());
+            }
+
+            JsonObjectBuilder job = Json.createObjectBuilder();
+            job.add("state","OK");
+            job.add("message","Successfully Loaded..!");
+            job.add("data",allItems.build());
+            resp.getWriter().print(job.build());
+
+        }catch (ClassNotFoundException | SQLException e){
+            JsonObjectBuilder rjo = Json.createObjectBuilder();
+            rjo.add("state","Error");
+            rjo.add("message",e.getLocalizedMessage());
+            rjo.add("data","");
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().print(rjo.build());
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String code = req.getParameter("code");
+        String description = req.getParameter("description");
+        String qtyOnHand = req.getParameter("qtyOnHand");
+        String unitPrice = req.getParameter("unitPrice");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/posapi", "root", "1234");
+
+            PreparedStatement pstm = connection.prepareStatement("insert into Item values(?,?,?,?)");
+            pstm.setObject(1,code);
+            pstm.setObject(2,description);
+            pstm.setObject(3,qtyOnHand);
+            pstm.setObject(4,unitPrice);
+            boolean b = pstm.executeUpdate() > 0;
+            if (b){
+                JsonObjectBuilder responseObject = Json.createObjectBuilder();
+                responseObject.add("state","Ok");
+                responseObject.add("message","Successfully added..!");
+                responseObject.add("data","");
+                resp.getWriter().print(responseObject.build());
+            }
+        } catch (ClassNotFoundException e) {
+            JsonObjectBuilder error = Json.createObjectBuilder();
+            error.add("state","Ok");
+            error.add("message",e.getLocalizedMessage());
+            error.add("data","");
+//            resp.setStatus(500);
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().print(error.build());
+        }catch (SQLException e) {
+            JsonObjectBuilder error = Json.createObjectBuilder();
+            error.add("state","Error");
+            error.add("message",e.getLocalizedMessage());
+            error.add("data","");
+//            resp.setStatus(400);
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().print(error.build());
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String code = req.getParameter("code");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/posapi", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("delete from Item where code=?");
+            pstm.setObject(1,code);
+            boolean b = pstm.executeUpdate() > 0;
+            if (b) {
+                JsonObjectBuilder rjo = Json.createObjectBuilder();
+                rjo.add("state","Ok");
+                rjo.add("message","Successfully Deleted..!");
+                rjo.add("data","");
+                resp.getWriter().print(rjo.build());
+            }else {
+                throw new RuntimeException("There is no Item for that ID..!");
+            }
+        } catch (RuntimeException e) {
+            JsonObjectBuilder rjo = Json.createObjectBuilder();
+            rjo.add("state","Error");
+            rjo.add("message",e.getLocalizedMessage());
+            rjo.add("data","");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().print(rjo.build());
+        }catch (ClassNotFoundException | SQLException e){
+            JsonObjectBuilder rjo = Json.createObjectBuilder();
+            rjo.add("state","Error");
+            rjo.add("message",e.getLocalizedMessage());
+            rjo.add("data","");
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().print(rjo.build());
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject item = reader.readObject();
+        String code = item.getString("code");
+        String description = item.getString("description");
+        String qtyOnHand = item.getString("qtyOnHand");
+        String unitPrice = item.getString("unitPrice");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/posapi", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("update Item set description=?,qtyOnHand=?,unitPrice=? where code=?");
+            pstm.setObject(4,code);
+            pstm.setObject(1,description);
+            pstm.setObject(2,qtyOnHand);
+            pstm.setObject(3,unitPrice);
+            boolean b = pstm.executeUpdate() > 0;
+            if (b){
+                JsonObjectBuilder responseObject = Json.createObjectBuilder();
+                responseObject.add("state","Ok");
+                responseObject.add("message","Successfully Updated..!");
+                responseObject.add("data","");
+                resp.getWriter().print(responseObject.build());
+            }else{
+                throw new RuntimeException("Wrong ID, Please check the ID..!");
+            }
+
+        } catch (RuntimeException e) {
+            JsonObjectBuilder rjo = Json.createObjectBuilder();
+            rjo.add("state","Error");
+            rjo.add("message",e.getLocalizedMessage());
+            rjo.add("data","");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().print(rjo.build());
+        }catch (ClassNotFoundException | SQLException e){
+            JsonObjectBuilder rjo = Json.createObjectBuilder();
+            rjo.add("state","Error");
+            rjo.add("message",e.getLocalizedMessage());
+            rjo.add("data","");
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().print(rjo.build());
+        }
     }
 }
--- FrontEnd/controller/ManageItemController.js
+++ FrontEnd/controller/ManageItemController.js
@@ -1,221 +1,127 @@
-$(function () {
-    $("form").submit(function () {
-        return false;
-    });
+const baseUrl = "http://localhost:8080/backend/";
 
-    // Auto Focus Item Code
-    $('#modelItem').on('shown.bs.modal', function () {
-        $('#txtItemCode').trigger('focus');
-    });
-    loadAllItems();
-});
+loadAllItems();
 
-// Button Save On Action
-$("#btnSaveItem").on("click", function () {
-    saveItem();
-});
+$("#btnSave").on("click", function () {
+    let formData = $("#itemForm").serialize();
 
-$("#btnAddNewItem").on("click", function () {
-    $("#btnSaveItem").text("Save");
-    $("#txtItemCode,#txtItemName,#txtItemPrice,#qtyOnHand").val("");
-    checkValidity(itemValidations);
-});
+    let json = {
+        code: $("#code").val(),
+        itemName: $("#itemName").val(),
+        qtyOnHand: parseInt($("#qtyOnHand").val()),
+        price: $("#price").val()
+    };
 
-function saveItem() {
-    let itemCode = $("#txtItemCode").val();
-    let itemName = $("#txtItemName").val();
-    let unitPrice = $("#txtItemPrice").val();
-    let qty = $("#qtyOnHand").val();
-
-    if ($("#btnSaveItem").text() == "Save") {
-        var item = setItem(itemCode, itemName, unitPrice, qty);
-        itemDB.push(item);
-        clearAllItemTexts();
-        saveAlert();
+    if ($(this).text() == "Save") {
+        $.ajax({
+            url: baseUrl + "item",
+            type: "post",
+            data: formData,
+            dataType: "json",
+            success: function (res) {
+                loadAllItems();
+                alert(res.message);
+            },
+            error: function (error) {  // When status code equals to 500 then, this method has been invoked
+                let parse = JSON.parse(error.responseText);
+                alert(parse.message);
+            }
+        });
     } else {
-        updateItem(itemCode, itemName, unitPrice, qty);
-        clearAllItemTexts();
-        updateAlert();
+        $.ajax({
+            url: baseUrl + "item",
+            method: "put",
+            contentType: "application/json",
+            data: JSON.stringify(json),
+            dataType: "json",
+            success: function (res) {
+                loadAllItems();
+                alert(res.message);
+            },
+            error: function (error) {  // When status code equals to 500 then, this method has been invoked
+                let parse = JSON.parse(error.responseText);
+                alert(parse.message);
+            }
+        });
     }
-    loadAllItems();
-}
 
-// Load all Items
-function loadAllItems() {
+});
 
-    $("#tblItem > tbody").empty();
+$("#btnAddItem").on("click", function () {
+    $("#btnSave").text("Save");
+});
 
-    for (let item of itemDB) {
-        $("#tblItem > tbody").append(
-            `<tr><td>${item.itemCode}</td><td>${item.itemName}</td><td>${item.unitPrice}</td><td>${item.qty}</td><td><i class="bi bi-pencil-fill text-success me-4 item-edits" data-bs-toggle="modal" data-bs-target="#modelItem"></i><i class="bi bi-trash text-danger item-deletes"></i></td></tr>`
-        );
-    }
-    bindItemsEditEvent();
-    bindItemsDeleteEvent();
-    loadAllItemCodesInPurchaseOrder();
-}
 
-// Edit button on action
-function bindItemsEditEvent() {
+function bindEditEvent() {
 
-    $(".item-edits").on("click", function () {
+    $(".customer-edits").on("click", function () {
         var code = $(this).parent().parent().children(":eq(0)").text();
 
         var name = $(this).parent().parent().children(":eq(1)").text();
 
-        var unitPrice = $(this).parent().parent().children(":eq(2)").text();
-
         var qty = $(this).parent().parent().children(":eq(3)").text();
 
-        setItemTextFields(code, name, unitPrice, qty);
-        $("#btnSaveItem").text("Update");
+        var price = $(this).parent().parent().children(":eq(2)").text();
+
+        setItemTextFields(code, name, price, qty);
+        $("#btnSave").text("Update");
+
     });
 }
 
-function setItemTextFields(code, name, unitPrice, qty) {
-    $("#txtItemCode").val(code);
-    $("#txtItemName").val(name);
-    $("#txtItemPrice").val(unitPrice);
-    $("#qtyOnHand").val(qty);
-}
+function bindDeleteEvent() {
+    $(".customer-deletes").on("click", function () {
 
-function bindItemsDeleteEvent() {
-    // Delete button on action
-    $(".item-deletes").on("click", function () {
-        var itemCode = $(this).parent().parent().children(":eq(0)").text();
-        let res = confirm("Are you sure..?");
-        if (res){
-            deleteItem(itemCode);
-            deleteAlert();
-        }
-        loadAllItems();
+        var code = $(this).parent().parent().children(":eq(0)").text();
+        console.log(code)
+
+        $.ajax({
+            url: baseUrl + "item?code=" + code.trim(),
+            method: "delete",
+            dataType: "json",
+            success: function (res) {
+                loadAllItems();
+                alert(res.message);
+            },
+            error: function (error) {  // When status code equals to 500 then, this method has been invoked
+                let parse = JSON.parse(error.responseText);
+                alert(parse.message);
+            }
+        });
+
     });
 }
 
-function deleteItem(itemCode) {
-    let item = searchItem(itemCode);
-    if (item != null) {
-        let indexNumber = itemDB.indexOf(item);
-        itemDB.splice(indexNumber, 1);
-        return true;
-    } else {
-        return false;
-    }
+function setItemTextFields(code, name, price, qty) {
+    $("#code").val(code.trim());
+    $("#itemName").val(name.trim());
+    $("#qtyOnHand").val(qty.trim());
+    $("#price").val(price.trim());
 }
 
-function searchItem(itemCode) {
-    for (let item of itemDB) {
-        if (item.itemCode == itemCode) {
-            return item;
-        }
-    }
-    return null;
-}
+$("#btnGetAll").on("click", function () {
+    loadAllCustomers();
+});
 
-function updateItem(itemCode, itemName, unitPrice, qty) {
-    let item = searchItem(itemCode);
-    if (item != null) {
-        item.itemCode = itemCode;
-        item.itemName = itemName;
-        item.unitPrice = unitPrice;
-        item.qty = qty;
-        return true;
-    } else {
-        return false;
-    }
-}
+function loadAllItems() {
+    $.ajax({
+        url: baseUrl + "item",
+        method: "get",
+        dataType: "json",
+        success: function (res) {
+            $("#body").empty();
 
-$('#txtItemSearch').on("keyup", function () {
-    $("#tblItem > tbody").empty();
-    for (let item of itemDB) {
-        if ($("#itemSearch").val()== "Item Code"){
-            if (item.itemCode.indexOf($("#txtItemSearch").val()) !== -1) {
-                let row = `<tr><td>${item.itemCode}</td><td>${item.itemName}</td><td>${item.unitPrice}</td><td>${item.qty}</td><td><i class="bi bi-pencil-fill text-success me-4 item-edits" data-bs-toggle="modal" data-bs-target="#modelItem"></i><i class="bi bi-trash text-danger item-deletes"></i></td></tr>`;
-                $("#tblItem > tbody").append(row);
+            for (let item of res.data) {
+
+                $("#body").append(`<tr><td>` + item.code + `</td><td>` + item.name + `</td><td>` + item.price + `</td><td>` + item.qty + `</td><td><button type="button" class="border border-0 customer-edits"><i class="bi bi-pencil-fill text-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop"></i></button><button class="border border-0 customer-deletes"><i class="bi bi-trash text-danger"></i></button></td></tr>`);
             }
-        }else{
-            if (item.itemName.indexOf($("#txtItemSearch").val()) !== -1) {
-                let row = `<tr><td>${item.itemCode}</td><td>${item.itemName}</td><td>${item.unitPrice}</td><td>${item.qty}</td><td><i class="bi bi-pencil-fill text-success me-4 item-edits" data-bs-toggle="modal" data-bs-target="#modelItem"></i><i class="bi bi-trash text-danger item-deletes"></i></td></tr>`;
-                $("#tblItem > tbody").append(row);
-            }
+            alert(res.message);
+            bindEditEvent();
+            bindDeleteEvent();
+        },
+        error: function (error) {  // When status code equals to 500 then, this method has been invoked
+            let parse = JSON.parse(error.responseText);
+            alert(parse.message);
         }
-    }
-    bindItemsEditEvent();
-    bindItemsDeleteEvent();
-});
-
-// Item Regular Expressions
-const itemCodeRegEx = /^(ITM-)[0-9]{1,3}$/;
-const itemNameRegEx = /^[A-z ]{4,20}$/;
-const unitPriceRegEx = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
-const qtyOnHandRegEx = /^[0-9]{1,}$/
-
-let itemValidations = [];
-itemValidations.push({reg: itemCodeRegEx, field: $('#txtItemCode'), error: 'Item Code Pattern is Wrong : ITM-001'});
-itemValidations.push({
-    reg: itemNameRegEx,
-    field: $('#txtItemName'),
-    error: 'Item Name Pattern is Wrong : A-z 5-20'
-});
-itemValidations.push({
-    reg: unitPriceRegEx,
-    field: $('#txtItemPrice'),
-    error: 'Item Price Pattern is Wrong : 100 or 100.00'
-});
-itemValidations.push({
-    reg: qtyOnHandRegEx,
-    field: $('#qtyOnHand'),
-    error: 'Qty Pattern is Wrong : 100'
-});
-
-$("#txtItemCode,#txtItemName,#txtItemPrice,#qtyOnHand").on('keyup', function (event) {
-    checkValidity(itemValidations);
-});
-
-$("#txtItemCode,#txtItemName,#txtItemPrice,#qtyOnHand").on('blur', function (event) {
-    checkValidity(itemValidations);
-});
-
-$("#txtItemCode").on('keydown', function (event) {
-    if (event.key == "Enter" && check(itemCodeRegEx, $("#txtItemCode"))) {
-        $("#txtItemName").focus();
-    } else {
-        focusText($("#txtItemCode"));
-    }
-});
-
-$("#txtItemName").on('keydown', function (event) {
-    if (event.key == "Enter" && check(itemNameRegEx, $("#txtItemName"))) {
-        $("#txtItemPrice").focus();
-    } else {
-        focusText($("#txtItemName"));
-    }
-});
-
-$("#txtItemPrice").on('keydown', function (event) {
-    if (event.key == "Enter" && check(unitPriceRegEx, $("#txtItemPrice"))) {
-        $("#qtyOnHand").focus();
-    } else {
-        focusText($("#txtItemPrice"));
-    }
-});
-
-$("#qtyOnHand").on('keydown', function (event) {
-    if (event.key == "Enter" && check(qtyOnHandRegEx, $("#qtyOnHand"))) {
-        let res = confirm("Do you want to add this item.?");
-        if (res) {
-            saveItem();
-            clearAllItemTexts(itemValidations);
-        }
-    }
-});
-
-$("#btnClearItem").on("click", function () {
-    clearAllItemTexts();
-})
-
-function clearAllItemTexts() {
-    $("#txtItemCode").focus();
-    $("#txtItemCode,#txtItemName,#txtItemPrice,#qtyOnHand").val("");
-    checkValidity(itemValidations);
+    });
 }
