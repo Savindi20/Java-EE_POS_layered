f96e9694360e53a36b921309a047b8334b1e7df0
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-08 14:40:41 +0530

    itemBOImpl refactored

--- BackEnd/src/lk/ijse/pos/bo/custom/ItemBO.java
+++ BackEnd/src/lk/ijse/pos/bo/custom/ItemBO.java
@@ -1,17 +1,19 @@
 package lk.ijse.pos.bo.custom;
 
 import lk.ijse.pos.dto.ItemDTO;
+
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface ItemBO {
-    ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException;
+    ArrayList<ItemDTO> getAllItems(Connection connection) throws SQLException, ClassNotFoundException;
 
-    void deleteItems(String id) throws SQLException, ClassNotFoundException;
+    void deleteItems(Connection connection, String id) throws SQLException, ClassNotFoundException;
 
-    void saveItem(ItemDTO itemDTO) throws SQLException, ClassNotFoundException;
+    void saveItem(Connection connection, ItemDTO itemDTO) throws SQLException, ClassNotFoundException;
 
-    void updateItem(ItemDTO itemDTO) throws SQLException, ClassNotFoundException;
+    void updateItem(Connection connection, ItemDTO itemDTO) throws SQLException, ClassNotFoundException;
 
-    boolean exitsItem(String id) throws SQLException, ClassNotFoundException;
+    boolean exitsItem(Connection connection, String id) throws SQLException, ClassNotFoundException;
 }
--- BackEnd/src/lk/ijse/pos/bo/custom/impl/ItemBOImpl.java
+++ BackEnd/src/lk/ijse/pos/bo/custom/impl/ItemBOImpl.java
@@ -2,34 +2,46 @@ package lk.ijse.pos.bo.custom.impl;
 
 import lk.ijse.pos.bo.SuperBO;
 import lk.ijse.pos.bo.custom.ItemBO;
+import lk.ijse.pos.dao.DAOFactory;
+import lk.ijse.pos.dao.custom.ItemDAO;
 import lk.ijse.pos.dto.ItemDTO;
+import lk.ijse.pos.entity.Item;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ItemBOImpl implements ItemBO, SuperBO {
+    ItemDAO itemDAO = (ItemDAO) DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.ITEM);
+
     @Override
-    public ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException {
-        return null;
+    public ArrayList<ItemDTO> getAllItems(Connection connection) throws SQLException, ClassNotFoundException {
+        ArrayList<Item> all = itemDAO.getAll(connection);
+        ArrayList<ItemDTO> allItems = new ArrayList<>();
+        for (Item item :
+                all) {
+            allItems.add(new ItemDTO(item.getCode(), item.getName(), item.getQty(), item.getPrice()));
+        }
+        return allItems;
     }
 
     @Override
-    public void deleteItems(String id) throws SQLException, ClassNotFoundException {
+    public void deleteItems(Connection connection, String id) throws SQLException, ClassNotFoundException {
 
     }
 
     @Override
-    public void saveItem(ItemDTO itemDTO) throws SQLException, ClassNotFoundException {
+    public void saveItem(Connection connection, ItemDTO itemDTO) throws SQLException, ClassNotFoundException {
 
     }
 
     @Override
-    public void updateItem(ItemDTO itemDTO) throws SQLException, ClassNotFoundException {
+    public void updateItem(Connection connection, ItemDTO itemDTO) throws SQLException, ClassNotFoundException {
 
     }
 
     @Override
-    public boolean exitsItem(String id) throws SQLException, ClassNotFoundException {
+    public boolean exitsItem(Connection connection, String id) throws SQLException, ClassNotFoundException {
         return false;
     }
 }
