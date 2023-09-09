69bce8aa9da39edf02e6de2710a25687f64dc76d
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-09 13:46:55 +0530

    orderDAOImpl refactored

--- BackEnd/src/lk/ijse/pos/dao/custom/OrderDAO.java
+++ BackEnd/src/lk/ijse/pos/dao/custom/OrderDAO.java
@@ -4,6 +4,8 @@ import lk.ijse.pos.dao.CrudDAO;
 import lk.ijse.pos.entity.Order;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
-public interface OrderDAO extends CrudDAO<Connection, Order, String>{
+public interface OrderDAO extends CrudDAO<Connection, Order, String> {
+    String generateNewOrderId(Connection connection) throws SQLException, ClassNotFoundException;
 }
--- BackEnd/src/lk/ijse/pos/dao/custom/impl/OrderDAOImpl.java
+++ BackEnd/src/lk/ijse/pos/dao/custom/impl/OrderDAOImpl.java
@@ -1,9 +1,11 @@
 package lk.ijse.pos.dao.custom.impl;
 
+import lk.ijse.pos.dao.SQLUtil;
 import lk.ijse.pos.dao.custom.OrderDAO;
 import lk.ijse.pos.entity.Order;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -14,8 +16,8 @@ public class OrderDAOImpl implements OrderDAO{
     }
 
     @Override
-    public boolean save(Connection connection, Order dto) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean save(Connection connection, Order entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.executeUpdate(connection, "INSERT INTO `Order` VALUES (?,?,?,?)", entity.getOrderId(), entity.getCusId(), entity.getCost(), entity.getOrderDate());
     }
 
     @Override
@@ -36,5 +38,11 @@ public class OrderDAOImpl implements OrderDAO{
     @Override
     public boolean delete(Connection connection, String s) throws SQLException, ClassNotFoundException {
         return false;
+    }
+
+    @Override
+    public String generateNewOrderId(Connection connection) throws SQLException, ClassNotFoundException {
+        ResultSet rst = SQLUtil.executeQuery(connection, "SELECT orderId FROM `Order` ORDER BY orderID DESC LIMIT 1");
+        return rst.next() ? String.format("OID%03d", (Integer.parseInt(rst.getString("orderID").replace("OID", "")) + 1)) : "OID001";
     }
 }
