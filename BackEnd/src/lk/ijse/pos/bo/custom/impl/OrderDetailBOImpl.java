e27f1a35be2f1594bba46972010b8e4abdcf7cfa
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-09 19:05:25 +0530

    Orderdetail getall refactored

--- BackEnd/src/lk/ijse/pos/bo/custom/impl/OrderDetailBOImpl.java
+++ BackEnd/src/lk/ijse/pos/bo/custom/impl/OrderDetailBOImpl.java
@@ -5,7 +5,9 @@ import lk.ijse.pos.bo.custom.OrderDetailBO;
 import lk.ijse.pos.dao.DAOFactory;
 import lk.ijse.pos.dao.custom.OrderDetailDAO;
 import lk.ijse.pos.dto.OrderDetailDTO;
+import lk.ijse.pos.entity.OrderDetail;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -14,7 +16,15 @@ public class OrderDetailBOImpl implements OrderDetailBO, SuperBO {
     private final OrderDetailDAO orderDetailDAO = (OrderDetailDAO) DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.ORDER_DETAILS);
 
     @Override
-    public ArrayList<OrderDetailDTO> getAllOrderDetails() throws SQLException, ClassNotFoundException {
-        return null;
+    public ArrayList<OrderDetailDTO> getAllOrderDetails(Connection connection) throws SQLException, ClassNotFoundException {
+
+        ArrayList<OrderDetail> all = orderDetailDAO.getAll(connection);
+        ArrayList<OrderDetailDTO> orderDetails = new ArrayList<>();
+
+        for (OrderDetail orderDetail : all) {
+            orderDetails.add(new OrderDetailDTO(orderDetail.getOrderId(), orderDetail.getCode(), orderDetail.getPrice(), orderDetail.getQty()));
+        }
+
+        return orderDetails;
     }
 }
