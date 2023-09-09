e5bd272075ff182bd9ed3b2d86a4ab8858c855c3
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-09-09 19:52:06 +0530

    layered completed

--- FrontEnd/model/Customer.js
+++ FrontEnd/model/Customer.js
@@ -1,8 +1,47 @@
-function setCustomer(customerId, name, address, salary) {
-    return {
-        cusId: customerId,
-        cusName: name,
-        cusAddress: address,
-        cusSalary: salary
-    };
+class Customer{
+    #cusId;
+    #cusName;
+    #cusAddress;
+    #cusSalary;
+
+    constructor(cusId, cusName, cusAddress, cusSalary) {
+        this.#cusId = cusId;
+        this.#cusName = cusName;
+        this.#cusAddress = cusAddress;
+        this.#cusSalary = cusSalary;
+    }
+
+    getCusId() {
+        return this.#cusId;
+    }
+
+    setCusId(value) {
+        this.#cusId = value;
+    }
+
+    getCusName() {
+        return this.#cusName;
+    }
+
+    setCusName(value) {
+        this.#cusName = value;
+    }
+
+    getCusAddress() {
+        return this.#cusAddress;
+    }
+
+    setCusAddress(value) {
+        this.#cusAddress = value;
+    }
+
+    getCusSalary() {
+        return this.#cusSalary;
+    }
+
+    setCusSalary(value) {
+        this.#cusSalary = value;
+    }
+
 }
+
